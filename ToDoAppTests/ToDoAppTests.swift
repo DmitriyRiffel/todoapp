ba13ab8e92e3ef6bb04525815c5e1080dd0d798d
DmitriyRiffel <dmitriyriffel1@gmail.com>
2025-04-22 10:50:55 +0200

    Initial Commit

---
+++ ToDoAppTests/ToDoAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppTests.swift
+//  ToDoAppTests
+//
+//  Created by Dmitriy Riffel on 22.04.25.
+//
+
+import Testing
+@testable import ToDoApp
+
+struct ToDoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
